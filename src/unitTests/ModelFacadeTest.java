795487138e52c06c5f44bdae2651def16da126c1
Matt <mtayl087@fiu.edu>
2020-03-03 09:08:56 -0500

    add setup/teardown for unit tests to ensure independence of tests

--- src/unitTests/ModelFacadeTest.java
+++ src/unitTests/ModelFacadeTest.java
@@ -7,6 +7,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import model.ModelFacade;
+import utilities.DBConnection;
+
+import java.rmi.ConnectException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ModelFacadeTest {
 
@@ -15,17 +21,261 @@ public class ModelFacadeTest {
 		// For setup, we add rows to tables to use to test methods that involve updating, deleting,
 		// or requesting data from those tables. We do this directly through DB.connection to avoid using
 		// the same code that we're testing
+		Connection con = DBConnection.createConnection();
+		Statement st = con.createStatement();
+				
+		// Set up database using the data from pmsdb.sql
+		String qry1 = "drop database if exists PMS;", 
+				qry2 = "create database PMS;",
+				qry3 = "use PMS;",
+				qry4 = "create table employees(" + 
+				"  emp_id VARCHAR(500)," + 
+				"  first_name VARCHAR(500)," + 
+				"  last_name VARCHAR(500)," + 
+				"  gender VARCHAR(500)," + 
+				"  dob DATE," + 
+				"  job VARCHAR(500)," + 
+				"  phone VARCHAR(500)," + 
+				"  email_id VARCHAR(500)," + 
+				"  address VARCHAR(500)," + 
+				"  accno VARCHAR(500)," + 
+				"  bankname VARCHAR(500)," + 
+				"  joindate DATE" + 
+				");",
+				qry5 = "create table employer(" + 
+				"  username VARCHAR(500)," + 
+				"  password VARCHAR(500)" + 
+				");",
+				qry6 = "create table users(" + 
+				"  emp_id VARCHAR(500)," + 
+				"  user_id VARCHAR(500)," + 
+				"  password VARCHAR(500)," + 
+				"  sec_que1 VARCHAR(500)," + 
+				"  ans1 VARCHAR(500)," + 
+				"  sec_que2 VARCHAR(500)," + 
+				"  ans2 VARCHAR(500)," + 
+				"  sec_que3 VARCHAR(500)," + 
+				"  ans3 VARCHAR(500)," + 
+				"  createDate DATE" + 
+				");",
+				qry7 = "create table paymode(" + 
+				"  normal_pay DOUBLE," + 
+				"  extra_pay DOUBLE" + 
+				");", 
+				qry8 = "create table emp_ts(" + 
+				"  ets_id VARCHAR(500)," + 
+				"  emp_id VARCHAR(500)," + 
+				"  day VARCHAR(500)," + 
+				"  wdate DATE," + 
+				"  intime TIME," + 
+				"  lunch_out TIME," + 
+				"  lunch_in TIME," + 
+				"  outtime TIME," + 
+				"  total_hours DOUBLE," + 
+				"  status VARCHAR(500)," + 
+				"  date1 DATE" + 
+				");",
+				qry9 = "create table save_ts(" + 
+				"  ets_id VARCHAR(500)," + 
+				"  emp_id VARCHAR(500)," + 
+				"  day VARCHAR(500)," + 
+				"  date1 DATE," + 
+				"  intime TIME," + 
+				"  lunch_out TIME," + 
+				"  lunch_in TIME," + 
+				"  outtime TIME," + 
+				"  total_hours DOUBLE," + 
+				"  status VARCHAR(500)," + 
+				"  createDate DATE" + 
+				");",
+				qry10 = "create table salaries(" + 
+				"  emp_id VARCHAR(500)," + 
+				"  total_hours DOUBLE," + 
+				"  tax DOUBLE," + 
+				"  gross_sal DOUBLE," + 
+				"  net_sal DOUBLE," + 
+				"  date1 DATE" + 
+				");",
+				qry11 = "INSERT INTO `employer` (username, password) VALUES ('user1', 'user1');",
+				qry12 = "INSERT INTO `users` (emp_id, user_id, password, createDate) VALUES ('1', 'user1', " + 
+				"'user1', '2020-01-12');",
+				qry13 = "INSERT INTO `employees` VALUES ('1','Adam','Sandler','on','1901-01-01','Movie " + 
+				"Star','0','adam.sandler@email.com','2121 SW 12TH ST','1','Bank of America','2020-" + 
+				"01-14');",
+				qry14 = "INSERT INTO `users` VALUES ('1','adam','adam','Favorite Color?','pink','First PEt " + 
+				"Name?','adam','Favorite movie?','adam','2020-01-14');", 
+				qry15 = "INSERT INTO `emp_ts` VALUES ('1144','1','Monday','2020-01-" + 
+				"01','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14')," + 
+				"('1114','1','Tuesday','2020-01-" + 
+				"02','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14')," + 
+				"('1172','1','Wednesday','2020-01-" + 
+				"03','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14')," + 
+				"('1341','1','Thursday','2020-01-" + 
+				"04','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14')," + 
+				"('1800','1','Friday','2020-01-" + 
+				"05','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14');", 
+				qry16 = "INSERT INTO `paymode` VALUES (10,15);",
+				qry17 = "INSERT INTO `salaries` VALUES ('1',55,165,550,385,'2020-01-14');";
 		
+		st.addBatch(qry1);
+		st.addBatch(qry2);
+		st.addBatch(qry3);
+		st.addBatch(qry4);
+		st.addBatch(qry5);
+		st.addBatch(qry6);
+		st.addBatch(qry7);
+		st.addBatch(qry8);
+		st.addBatch(qry9);
+		st.addBatch(qry10);
+		st.addBatch(qry11);
+		st.addBatch(qry12);
+		st.addBatch(qry13);
+		st.addBatch(qry14);
+		st.addBatch(qry15);
+		st.addBatch(qry16);
+		st.addBatch(qry17);
+		
+		int[] res = st.executeBatch();
+		
+		// Make sure all responses are good
+		for (int i : res) {
+			if (i == Statement.EXECUTE_FAILED)
+				throw new Exception("Error while setting up database for tests, please check statements.");
+		}
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		// For setup, we add rows to tables to use to test methods that involve updating, deleting,
+		// or requesting data from those tables. We do this directly through DB.connection to avoid using
+		// the same code that we're testing
+		Connection con = DBConnection.createConnection();
+		Statement st = con.createStatement();
+				
+		// Set up database using the data from pmsdb.sql
+		String qry1 = "drop database if exists PMS;", 
+				qry2 = "create database PMS;",
+				qry3 = "use PMS;",
+				qry4 = "create table employees(" + 
+				"  emp_id VARCHAR(500)," + 
+				"  first_name VARCHAR(500)," + 
+				"  last_name VARCHAR(500)," + 
+				"  gender VARCHAR(500)," + 
+				"  dob DATE," + 
+				"  job VARCHAR(500)," + 
+				"  phone VARCHAR(500)," + 
+				"  email_id VARCHAR(500)," + 
+				"  address VARCHAR(500)," + 
+				"  accno VARCHAR(500)," + 
+				"  bankname VARCHAR(500)," + 
+				"  joindate DATE" + 
+				");",
+				qry5 = "create table employer(" + 
+				"  username VARCHAR(500)," + 
+				"  password VARCHAR(500)" + 
+				");",
+				qry6 = "create table users(" + 
+				"  emp_id VARCHAR(500)," + 
+				"  user_id VARCHAR(500)," + 
+				"  password VARCHAR(500)," + 
+				"  sec_que1 VARCHAR(500)," + 
+				"  ans1 VARCHAR(500)," + 
+				"  sec_que2 VARCHAR(500)," + 
+				"  ans2 VARCHAR(500)," + 
+				"  sec_que3 VARCHAR(500)," + 
+				"  ans3 VARCHAR(500)," + 
+				"  createDate DATE" + 
+				");",
+				qry7 = "create table paymode(" + 
+				"  normal_pay DOUBLE," + 
+				"  extra_pay DOUBLE" + 
+				");", 
+				qry8 = "create table emp_ts(" + 
+				"  ets_id VARCHAR(500)," + 
+				"  emp_id VARCHAR(500)," + 
+				"  day VARCHAR(500)," + 
+				"  wdate DATE," + 
+				"  intime TIME," + 
+				"  lunch_out TIME," + 
+				"  lunch_in TIME," + 
+				"  outtime TIME," + 
+				"  total_hours DOUBLE," + 
+				"  status VARCHAR(500)," + 
+				"  date1 DATE" + 
+				");",
+				qry9 = "create table save_ts(" + 
+				"  ets_id VARCHAR(500)," + 
+				"  emp_id VARCHAR(500)," + 
+				"  day VARCHAR(500)," + 
+				"  date1 DATE," + 
+				"  intime TIME," + 
+				"  lunch_out TIME," + 
+				"  lunch_in TIME," + 
+				"  outtime TIME," + 
+				"  total_hours DOUBLE," + 
+				"  status VARCHAR(500)," + 
+				"  createDate DATE" + 
+				");",
+				qry10 = "create table salaries(" + 
+				"  emp_id VARCHAR(500)," + 
+				"  total_hours DOUBLE," + 
+				"  tax DOUBLE," + 
+				"  gross_sal DOUBLE," + 
+				"  net_sal DOUBLE," + 
+				"  date1 DATE" + 
+				");",
+				qry11 = "INSERT INTO `employer` (username, password) VALUES ('user1', 'user1');",
+				qry12 = "INSERT INTO `users` (emp_id, user_id, password, createDate) VALUES ('1', 'user1', " + 
+				"'user1', '2020-01-12');",
+				qry13 = "INSERT INTO `employees` VALUES ('1','Adam','Sandler','on','1901-01-01','Movie " + 
+				"Star','0','adam.sandler@email.com','2121 SW 12TH ST','1','Bank of America','2020-" + 
+				"01-14');",
+				qry14 = "INSERT INTO `users` VALUES ('1','adam','adam','Favorite Color?','pink','First PEt " + 
+				"Name?','adam','Favorite movie?','adam','2020-01-14');", 
+				qry15 = "INSERT INTO `emp_ts` VALUES ('1144','1','Monday','2020-01-" + 
+				"01','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14')," + 
+				"('1114','1','Tuesday','2020-01-" + 
+				"02','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14')," + 
+				"('1172','1','Wednesday','2020-01-" + 
+				"03','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14')," + 
+				"('1341','1','Thursday','2020-01-" + 
+				"04','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14')," + 
+				"('1800','1','Friday','2020-01-" + 
+				"05','10:00:00','12:00:00','13:00:00','22:00:00',11,'approved','2020-01-14');", 
+				qry16 = "INSERT INTO `paymode` VALUES (10,15);",
+				qry17 = "INSERT INTO `salaries` VALUES ('1',55,165,550,385,'2020-01-14');";
+		
+		st.addBatch(qry1);
+		st.addBatch(qry2);
+		st.addBatch(qry3);
+		st.addBatch(qry4);
+		st.addBatch(qry5);
+		st.addBatch(qry6);
+		st.addBatch(qry7);
+		st.addBatch(qry8);
+		st.addBatch(qry9);
+		st.addBatch(qry10);
+		st.addBatch(qry11);
+		st.addBatch(qry12);
+		st.addBatch(qry13);
+		st.addBatch(qry14);
+		st.addBatch(qry15);
+		st.addBatch(qry16);
+		st.addBatch(qry17);
+		
+		int[] res = st.executeBatch();
+		
+		// Make sure all responses are good
+		for (int i : res) {
+			if (i == Statement.EXECUTE_FAILED)
+				throw new Exception("Error while setting up database for tests, please check statements.");
+		}
 	}
 
 	@Test
 	public void testTimeSheetaddTimeSheet() {
 		// Valid data, expect success
-	    String result = ModelFacade.TimeSheetaddTimeSheet("1", "1", "2020-03-02", 
+	    String result = ModelFacade.TimeSheetaddTimeSheet("1", "1", "Monday", 
 	    		"2020-03-02", "09:00:00", "12:00:00", "13:00:00", "17:00:00");
 	    
 		assertEquals("success", result);

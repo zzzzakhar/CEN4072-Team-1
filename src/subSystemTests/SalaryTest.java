55ef395f3069cb12c7663ebd32222bd1c287f1cd
Nicholas <Nicholas@SM-NICK-GAMING-PC.attlocal.net>
2020-04-14 18:57:02 -0400

    Salary test start, 5.2% coverage

--- src/subSystemTests/SalaryTest.java
+++ src/subSystemTests/SalaryTest.java
@@ -1,29 +1,39 @@
 package subSystemTests;
 
+import java.sql.*;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import model.Salary;
 
 public class SalaryTest {
 
 	@Test
 	public void testCalculateSalary() {
-		Salary test = new Salary();
+		String test = new Salary().calculateSalary();
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void testGetEmpPays() {
+		ResultSet test = new Salary().getEmpPays();
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testGetEmpPay(String empid) {
+	public void testGetEmpPay() {
+		String empid = "";
+		ResultSet test = new Salary().getEmpPay(empid);
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testAddPayMode(double normal, double extra) {
+	public void testAddPayMode() {
+		double normal = 0;
+		double extra = 0;
+		String test = new Salary().addPayMode(normal,extra);
 		fail("Not yet implemented");
 	}
 

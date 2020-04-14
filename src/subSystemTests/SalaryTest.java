2b5ae2ba236dbcb084a54823b265c81712c648bd
Nicholas <Nicholas@SM-NICK-GAMING-PC.attlocal.net>
2020-04-14 18:49:22 -0400

    Trying to do test cases for salary sub system class.

---
+++ src/subSystemTests/SalaryTest.java
@@ -0,0 +1,30 @@
+package subSystemTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SalaryTest {
+
+	@Test
+	public void testCalculateSalary() {
+		Salary test = new Salary();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetEmpPays() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetEmpPay(String empid) {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAddPayMode(double normal, double extra) {
+		fail("Not yet implemented");
+	}
+
+}

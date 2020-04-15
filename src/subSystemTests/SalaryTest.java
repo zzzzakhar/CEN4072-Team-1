b494fe928d03875ceff6d9bec651e1e69c8edff7
Nicholas <Nicholas@SM-NICK-GAMING-PC.attlocal.net>
2020-04-14 23:06:57 -0400

    Salary test cases, 8.1% coverage

--- src/subSystemTests/SalaryTest.java
+++ src/subSystemTests/SalaryTest.java
@@ -6,35 +6,80 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import model.Salary;
+import model.ModelFacade;
+import utilities.DBConnection;
 
 public class SalaryTest {
+	
+	private ResultSet resultSet = null;
 
 	@Test
 	public void testCalculateSalary() {
-		String test = new Salary().calculateSalary();
-		fail("Not yet implemented");
+		Connection con = DBConnection.createConnection();
+		try {
+			Statement statement = con.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String test = ModelFacade.SalarycalculateSalary();
 	}
 	
 	@Test
 	public void testGetEmpPays() {
-		ResultSet test = new Salary().getEmpPays();
-		fail("Not yet implemented");
+		try {
+			Connection con = DBConnection.createConnection();
+			Statement statement = con.createStatement();
+			resultSet = ModelFacade.SalarygetEmpPays();
+			
+			resultSet.next();
+			
+			assertTrue(resultSet.getString("emp_id").equals("1"));
+			assertTrue(resultSet.getString("total_hours").equals("55"));
+			assertTrue(resultSet.getString("tax").equals("165"));
+			assertTrue(resultSet.getString("gross_sal").equals("550"));
+			assertTrue(resultSet.getString("net_sal").equals("385"));
+			assertTrue(resultSet.getString("date1").equals("2020-01-14"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void testGetEmpPay() {
-		String empid = "";
-		ResultSet test = new Salary().getEmpPay(empid);
-		fail("Not yet implemented");
+		try {
+			Connection con = DBConnection.createConnection();
+			Statement statement = con.createStatement();
+			String empid = "1";
+			resultSet = ModelFacade.SalarygetEmpPay(empid);
+			
+			resultSet.next();
+			
+			assertTrue(resultSet.getString("emp_id").equals("1"));
+			assertTrue(resultSet.getString("total_hours").equals("55"));
+			assertTrue(resultSet.getString("tax").equals("165"));
+			assertTrue(resultSet.getString("gross_sal").equals("550"));
+			assertTrue(resultSet.getString("net_sal").equals("385"));
+			assertTrue(resultSet.getString("date1").equals("2020-01-14"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void testAddPayMode() {
+		Connection con = DBConnection.createConnection();
+		try {
+			Statement statement = con.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		double normal = 0;
 		double extra = 0;
-		String test = new Salary().addPayMode(normal,extra);
-		fail("Not yet implemented");
+		String test = ModelFacade.SalaryaddPayMode(normal,extra);
 	}
 
 }
